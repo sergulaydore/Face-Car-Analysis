8f9cefff806b02c1527dc715640e4bdc087a82f2
sergulaydore <sergulaydore@gmail.com>
2015-03-10 12:44:39 -0400

    theano LR still problematic

--- BLR_LOO_FC_theano.py
+++ BLR_LOO_FC_theano.py
@@ -13,51 +13,6 @@ from sklearn.cross_validation import cross_val_score
 import theano
 import theano.tensor as T
 
-sub = 1 # subject number
-cohlevel = 30 # coherence level
-FC = 'FC' # face/car trials
-path = '../FC_Mario/rawdata/' # aaron25jun04/events_aaron25jun04.mat'
-
-subjects = loadmat(path + 'subjects.mat')
-my_subject = 'jeremy15jul04'
-
-gaincorrect=1;
-Fs=1000;
-StartOffset=0 #-200;
-duration=1000-StartOffset;
-Unit=1e7;
-
-print 'Loading pre-processed data' + '-'*10
-path_preprocessed = '../preprocessed_for_python/' + my_subject + '/' + 'Coh' + str(cohlevel) + '.mat'
-EEG = loadmat(path_preprocessed)
-       # keys: 'eeg_face', 'eeg_car'
-       # np.shape(EEG['EEG_face']) = 60 x 1200 x 30
-       # EEG['EEG_car'].size = 60 x 1200 x 40
-
-print 'Initialization' + '-'*10
-chan = np.shape(EEG['EEG_face'])[0]
-tmin = StartOffset
-timebin_onset = range(0, 150, 50)
-timebin_onset.extend(range(150,460,10))
-timebin_onset.extend(range(500,750,50))
-L_timebin = 30 # length of the timebin (ms)
-Nsample = int(round(L_timebin/float(1000)*Fs))
-Nface = np.shape(EEG['EEG_face'])[2]*Nsample
-Ncar = np.shape(EEG['EEG_car'])[2]*Nsample
-
-# LOO loop
-tt =0
-print ('LR using time bin %s - %s ms ...')%(timebin_onset[tt],timebin_onset[tt]+L_timebin)
-x1 = int(round((timebin_onset[tt]-tmin)*Fs/1000));
-xbin = x1 + np.arange(Nsample)
-data1 = EEG['EEG_face'][:,xbin,:].reshape(chan, Nface) 
-data1 = np.transpose(data1) # 900 x 60
-data2 = EEG['EEG_car'][:,xbin,:].reshape(chan, Ncar) 
-data2 = np.transpose(data2) # 1200 x 60
-X_eeg = np.vstack((data1,data2))
-y_eeg = np.append(np.ones(Nface, dtype=int), np.zeros(Ncar))
-n_features = np.shape(X_eeg)[1]
-
 class LogisticRegression_theano(object):
     """ Multi-class Logistic Regression Class
     
@@ -67,7 +22,7 @@ class LogisticRegression_theano(object):
     a class membership probability.
     """
     
-    def __init__(self, input, n_in):
+    def __init__(self, input, n_in, rng):
         """ Initialize the parameters of the logistic regression
         
         :type input: theano.tensor.TensorType
@@ -81,11 +36,21 @@ class LogisticRegression_theano(object):
                       which the labels lie
         """
         # initialize with 0 the weights as a matrix of shape (n_in, n_out)
-        self.W = theano.shared(
-            value = np.zeros(n_in, dtype=float),
-            name = 'W',
-            borrow = True
+#        self.W = theano.shared(
+#            value = np.zeros(n_in, dtype=float),
+#            name = 'W',
+#            borrow = True
+#        )
+        
+        W_values = np.asarray(
+            rng.uniform(
+                low = -4*np.sqrt(6. / (n_in + 2)),
+                high = 4*np.sqrt(6. / (n_in + 2) ),
+                size = (n_in,)
+            )
         )
+
+        self.W = theano.shared(value = W_values, name = 'W', borrow=True)        
         
         # initialize the biases b as a vector of n_out 0s
         self.b = theano.shared(
@@ -136,6 +101,9 @@ class LogisticRegression_theano(object):
         self.xent = -y * T.log(self.p1) - (1-y) * T.log(1-self.p1)
         return self.xent.mean()
         
+    def l2(self):
+        return T.sum(self.W ** 2)
+        
     def errors(self, y):
         """ Return a float representing the number of errors in the minibatch
         over the total number of examples of the minibatch; zero one 
@@ -160,6 +128,40 @@ class LogisticRegression_theano(object):
         #else:
         #    raise NotImplementedError()
 
+cohlevel = 45 # coherence level
+FC = 'FC' # face/car trials
+path = '../FC_Mario/rawdata/' # aaron25jun04/events_aaron25jun04.mat'
+
+subjects = loadmat(path + 'subjects.mat')
+my_subject = 'paul21apr04'
+
+gaincorrect=1;
+Fs=1000;
+StartOffset=0 #-200;
+duration=1000-StartOffset;
+Unit=1e7;
+
+print 'Loading pre-processed data' + '-'*10
+path_preprocessed = '../preprocessed_for_python/' + my_subject + '/' + 'Coh' + str(cohlevel) + '.mat'
+EEG = loadmat(path_preprocessed)
+       # keys: 'eeg_face', 'eeg_car'
+       # np.shape(EEG['EEG_face']) = 60 x 1200 x 30
+       # EEG['EEG_car'].size = 60 x 1200 x 40
+
+print 'Initialization' + '-'*10
+chan = np.shape(EEG['EEG_face'])[0]
+tmin = StartOffset
+timebin_onset = range(0, 150, 50)
+timebin_onset.extend(range(150,460,50))
+timebin_onset.extend(range(500,750,50))
+L_timebin = 30 # length of the timebin (ms)
+Nsample = int(round(L_timebin/float(1000)*Fs))
+Nface = np.shape(EEG['EEG_face'])[2]*Nsample
+Ncar = np.shape(EEG['EEG_car'])[2]*Nsample
+EEG1 = EEG['EEG_face']
+EEG2 = EEG['EEG_car']
+n_features = chan
+
 """ Generate symbolic variables for input (X and y
 represent a minibatch)
 """
@@ -167,11 +169,13 @@ X = T.matrix('X') # 2100 x 60 data
 y = T.vector('y') # labels, presented as 1D vector of [int] labels
 
 """ Construct the logistic regression class """
-classifier = LogisticRegression_theano(input=X, n_in=n_features)
-
+rng = np.random.RandomState(1234)
+classifier = LogisticRegression_theano(input=X, n_in=n_features, rng=rng) 
+          
 """ The cost we minimize during training is the negative likelihood
 of model in symbolic format """
-cost = classifier.negative_log_likelihood(y)
+lambda_2 = 1/1e6
+cost = classifier.negative_log_likelihood(y) + lambda_2*classifier.l2()
 
 """ BUILDING MODEL"""
 
@@ -181,7 +185,7 @@ g_W = T.grad(cost=cost, wrt=classifier.W)
 g_b = T.grad(cost=cost, wrt=classifier.b)
 
 # specify how to update the parameters of the model 
-learning_rate = 0.01
+learning_rate = 0.001
 updates = [(classifier.W, classifier.W - learning_rate * g_W),
            (classifier.b, classifier.b - learning_rate * g_b)]
 
@@ -196,7 +200,7 @@ train_model = theano.function(
 
 """ Testing the Model"""
 test_model = theano.function(
-    inputs = [X,y],
+    inputs = [X, y],
     outputs = classifier.errors(y)
 )
 
@@ -206,86 +210,64 @@ predict = theano.function(
 )
 
 """ Leave One Out """
-Ntrial = (Nface+Ncar)/Nsample
-
-from sklearn.cross_validation import LeaveOneOut
-loo = LeaveOneOut(Ntrial)
-
-downsampled = np.array(range(0,np.shape(X_eeg)[0], Nsample))
-predictions = []
-training_steps = 10
-cost_all = []
-err_all=[]
-for train, test in loo:
-    print test
+acc = []
+# LOO loop
+for time_point in timebin_onset:
     
-    Xtrain = X_eeg[downsampled[train[:]],:]
-    ytrain = y_eeg[downsampled[train[:]]]
-    Xtest = X_eeg[downsampled[test[:]],:]
-    ytest = y_eeg[downsampled[test[:]]]
+    print ('LR using time bin %s - %s ms ...')%(time_point,time_point+L_timebin)
+    x1 = int(round((time_point-tmin)*Fs/1000));
+    xbin = x1 + np.arange(Nsample)
+    data1 = np.transpose(EEG1[:,xbin,0:Nface+1]).reshape(Nface, chan)  # 900 x 60
+    data2 = np.transpose(EEG2[:,xbin,0:Ncar+1]).reshape(Ncar, chan)# 1200 x 60
+    X_eeg = np.vstack((data1,data2))
+    y_eeg = np.append(np.ones(Nface, dtype=int), np.zeros(Ncar))
     
-    classifier.W.set_value(np.zeros(n_features, dtype=float))
-    classifier.b.set_value(0.)
+    Ntrial = (Nface+Ncar)/Nsample
     
-    cost_single = []
-    for idx in range(training_steps):
-        cost = train_model(Xtrain, ytrain)
-        cost_single.append(cost)
+    predictions = []
+    training_steps = 10000
+    cost_all = []
+    err_all=[]
+    
+    for k in range(Ntrial):
+        LOO_index = range(k*Nsample, (k+1)*Nsample)
+        train_index = list(set(range(Nface+Ncar))-set(LOO_index)) # remove one trial from dataset
+    
+        Xtrain = X_eeg[train_index,:]
+        ytrain = y_eeg[train_index]
+        Xtest = np.mean(X_eeg[LOO_index,:],0).reshape(1, chan)
+        ytest = y_eeg[LOO_index[0:1]]
         
-    single_pred = predict(Xtest)
-    predictions.append(single_pred)    
-    cost_all.append(cost_single)
-    err = test_model(Xtest, ytest)
-    err_all.append(err)
+        classifier.W.set_value(np.asarray(
+            rng.uniform(
+                low = -4*np.sqrt(6. / (n_features + 2)),
+                high = 4*np.sqrt(6. / (n_features + 2) ),
+                size = (n_features,)
+            )
+        ))
+        classifier.b.set_value(0.)
+        cost_single = []
+        for idx in range(training_steps):
+            cost = train_model(Xtrain, ytrain)
+            cost_single.append(cost)
+            
+        single_pred = predict(Xtest)
+        predictions.append(single_pred)    
+        cost_all.append(cost_single)
+        err = test_model(Xtest, ytest)
+        err_all.append(err)
       
-predictions
+    acc.append( accuracy_score(y_eeg[range(0,np.shape(X_eeg)[0], Nsample)],predictions) ) # scikit's accuracy_score function
+                                                           # computes Accuracy classification score
+                                                           # note that I had to downsample y
+                                                           # because I don't need all 2100 samples
+                                                         # I only need 70 trials  
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-
-
-
-
-
-
-
-
-
-
-#beta, y_LOO ,LOO = [], [], [] # we probably don't need this anymore
-#                              # thanks to scikit library
-#acc, predictions, confidence_scores = [], [], []
-#for k in range(Ntrial):
-#    print k
-#    LOO_index = range(k*Nsample, (k+1)*Nsample)
-#    train_index = list(set(range(Nface+Ncar))-set(LOO_index)) # remove one trial from dataset
-#                                                              # sets are cool to perform set like operations
-#                                                              # Here we find A-B for sets A and B
-#    # instantiate a logistic regression model, and fit with X and y
-#    model = LogisticRegression(penalty='l2', C=100000000000000) # cool things start here
-#                                 # scikit is a popular machine leraning library in python
-#                                 # good to know this but we will later use theano
-#                                 # for more complex algorithms
-#    model = model.fit(X[train_index,:], y[train_index])
-#    confidence_scores.append( model.decision_function(np.mean(X[LOO_index,:],0)) )
-#                             # The confidence score for a sample is the signed
-#                             # distance of that sample to the hyperplane.  
-#    single_pred = model.predict(np.mean(X[LOO_index,:],0)) 
-#    print single_pred
-#    del model
-#    predictions.append( single_pred ) # compute predictions
-#                                                                   # we use average over 30 samples
-# 
-#acc.append( accuracy_score(y[range(0,np.shape(X)[0], Nsample)],predictions) ) # scikit's accuracy_score function
-#                                                           # computes Accuracy classification score
-#                                                           # note that I had to downsample y
-#                                                           # because I don't need all 2100 samples
-#                                                           # I only need 70 trials
-#
-#fpr, tpr, thresholds = roc_curve(y[range(0,2100, Nsample)], confidence_scores)
-#auc = auc(fpr,tpr)
-#
-#
-## evaluate the model using 10-fold cross-validation
-#scores = cross_val_score(LogisticRegression(), X, y, scoring='accuracy', cv=10)
-#print scores
-#print scores.mean()
+sns.axes_style("darkgrid")
+plt.plot(timebin_onset, acc, sns.xkcd_rgb["pale red"], lw=3)
+plt.ylabel('Accuracies',fontsize=14)
+plt.xlabel('time(msec)',fontsize=14)
+plt.title('Leave One Out ; Subject ' + my_subject + '; Coherence level ' + str(cohlevel))    
